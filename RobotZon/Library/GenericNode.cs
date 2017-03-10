115bcbcaada5c81497dc76ce831730574fa4ba80
corbienassisdev <corentin.bienassis@gmail.com>
2017-03-10 11:00:11 +0100

    ajout classe NodeWarehouse

--- RobotZon/Library/GenericNode.cs
+++ RobotZon/Library/GenericNode.cs
@@ -2,24 +2,24 @@
 
 namespace RobotZon.Salotti
 {
-    public abstract class Node
+    public abstract class GenericNode
     {
         protected string Name;                          //Doit être unique pour chaque Node !
         public double GCost { get; set; }               //Coût du chemin du noeud initial jusqu'à ce noeud
         public double HCost { get; set; }               //Estimation heuristique du coût pour atteindre le noeud final
         public double TotalCost { get; set; }               //Estimation heuristique du coût pour atteindre le noeud final
 
-        public Node Parent { get; protected set; }
-        public List<Node> Children { get; protected set; }
+        public GenericNode Parent { get; protected set; }
+        public List<GenericNode> Children { get; protected set; }
 
-        public Node(string name)
+        public GenericNode(string name)
         {
             Name = name;
             Parent = null;
-            Children = new List<Node>();
+            Children = new List<GenericNode>();
         }
 
-        public void AddParent(Node parent)
+        public void AddParent(GenericNode parent)
         {
             Parent = parent;
             if (Parent != null)
@@ -42,10 +42,10 @@ namespace RobotZon.Salotti
             return GCost + HCost;
         }
 
-        public abstract bool IsEqual(Node other);
-        public abstract double GetArcCost(Node other);
+        public abstract bool IsEqual(GenericNode other);
+        public abstract double GetArcCost(GenericNode other);
         public abstract bool EndState();
-        public abstract List<Node> GetListSucc();
+        public abstract List<GenericNode> GetListSucc();
         public abstract void CalculeHCost();
         //On peut aussi penser à surcharger ToString() pour afficher correctement un état
     }

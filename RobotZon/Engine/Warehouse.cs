d625f73b73a2f6a18b0ac2f73b13cb64e65e531b
Thibaud_Laurent <thibaud.laurent6@gmail.com>
2017-03-16 09:46:17 +0100

    Ajout du graphe dans Warehouse et début sa construction.

--- RobotZon/Engine/Warehouse.cs
+++ RobotZon/Engine/Warehouse.cs
@@ -1,4 +1,5 @@
-﻿namespace RobotZon.Engine
+﻿using System.Collections.Generic;
+namespace RobotZon.Engine
 {
     public class Warehouse
     {
@@ -6,11 +7,61 @@
         public Robot[] Robots { get; set; }
         public Item[] Items { get; set; }
 
+        public NodeWarehouse[,] Graph { get; set; }
+
         public Warehouse(int[,] data, Robot[] robots, Item[] items)
         {
             Data = data;
             Robots = robots;
             Items = items;
+
+            Graph = new NodeWarehouse[Data.GetLength(0), Data.GetLength(1)];
+            for (int r = 0; r < Data.GetLength(0); r++)
+            {
+                for (int c = 0; c < Data.GetLength(1); c++)
+                {
+                    List<Position> neighbours = new List<Position>();
+
+                    int n = GetData(r, c);
+                    switch (n)
+                    {
+                        case 0:
+                            if (GetData(r + 1, c) == -1 || GetData(r + 1, c) == 0)
+                            {
+                                neighbours.Add(new Position(r + 1, c));
+                            }
+
+                            if (GetData(r - 1, c) == 0)
+                            {
+                                neighbours.Add(new Position(r - 1, c));
+                            }
+
+                            if (GetData(r, c + 1) == 0)
+                            {
+                                neighbours.Add(new Position(r, c + 1));
+                            }
+
+                            if (GetData(r, c - 1) == 0)
+                            {
+                                neighbours.Add(new Position(r, c - 1));
+                            }
+                            break;
+                    }
+                    int a = 2;
+                }
+            }
+        }
+
+        public int GetData(int row, int column)
+        {
+            int res = -2;
+
+            if (row >= 0 && row < Data.GetLength(0) && column >= 0 && column < Data.GetLength(1))
+            {
+                res = Data[row, column];
+            }
+
+            return res;
         }
     }
 }
